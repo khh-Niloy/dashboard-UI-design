8bb722d0f9858588e761365269417f589bbd254f
khh-Niloy <niloy.dev.101@gmail.com>
2024-08-10 20:22:28 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        Poppins: ['Poppins', 'sans-serif']
+      },
+      colors: {
+        primary: '#5c6aed',
+      },
+    },
+    
+  },
+  plugins: [],
+}
+
